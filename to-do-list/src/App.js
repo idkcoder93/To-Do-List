ee197a7604fbcabd7db29461769be9fe28c4a89d
idkcoder93 <afridman0793@conestogac.on.ca>
2023-12-16 18:34:37 -0500

    Initialized react app

---
+++ to-do-list/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello chris this where we build our code!!!</h1>
+    </div>
+  );
+}
+
+export default App;
